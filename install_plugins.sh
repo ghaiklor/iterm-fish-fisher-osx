8b4fa68f5f730d0c8325f84be26eb1f836c3dce9
Eugene Obrezkov <ghaiklor@gmail.com>
2017-12-11 15:16:43 +0200

    Update install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -11,12 +11,12 @@
 # Plugins Configuration
 brew install jq grc thefuck
 omf install spark license battery git-flow await hash errno brew node-binpath grc pj thefuck
-set -gx PROJECT_PATHS ~/Library/Projects
+set -U PROJECT_PATHS ~/Library/Projects
 
 # Theme Configuration
 brew install --with-default-names gnu-sed
 brew install coreutils
-set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
 set -U fish_key_bindings fish_vi_key_bindings
 set -U budspencer_nogreeting
 fish_update_completions
