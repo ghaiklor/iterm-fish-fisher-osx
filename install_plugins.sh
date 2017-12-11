9e942fb81953127ffaf9f29139ef866c161eeaa4
Eugene Obrezkov <ghaiklor@gmail.com>
2017-12-11 13:16:36 +0200

    Update install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -8,14 +8,14 @@
 #fish_version   :2.7.0
 #===================================================================================
 
+# Plugins Configuration
+brew install jq grc thefuck
+omf install spark license battery git-flow await hash errno brew node-binpath grc pj thefuck
+set -gx PROJECT_PATHS ~/Library/Projects
+
 # Theme Configuration
 brew install --with-default-names gnu-sed
 set -U fish_key_bindings fish_vi_key_bindings
 set -U budspencer_nogreeting
 fish_update_completions
 omf install budspencer
-
-# Plugins Configuration
-brew install jq grc thefuck
-omf install spark license battery git-flow await hash errno brew node-binpath grc pj thefuck
-set -gx PROJECT_PATHS ~/Library/Projects
