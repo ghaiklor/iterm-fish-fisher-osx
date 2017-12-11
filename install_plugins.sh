38ccd7ce45835fc3c6b2790b94ec9925a6a3457c
Eugene Obrezkov <ghaiklor@gmail.com>
2017-12-11 11:16:37 +0200

    Update plugins installer

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 #title          :install_plugins.sh
-#description    :This script will install and configure plugins for Oh My Fish.
+#description    :This script will install and configure themes and plugins for Oh My Fish.
 #author         :ghaiklor
 #date           :2017-12-11
 #version        :0.1
@@ -8,7 +8,13 @@
 #fish_version   :2.7.0
 #===================================================================================
 
+# Theme Configuration
+brew install --with-default-names gnu-sed
+set -U fish_key_bindings fish_vi_key_bindings
+fish_update_completions
+omf install budspencer
+
+# Plugins Configuration
 brew install jq grc thefuck
-omf install bobthefish
 omf install spark license battery git-flow await hash errno brew node-binpath grc pj thefuck
 set -gx PROJECT_PATHS ~/Library/Projects
