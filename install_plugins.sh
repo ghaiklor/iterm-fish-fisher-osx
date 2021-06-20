5c13a9a8e8db001985d39bf87ede3fbcec15ee91
Eugene Obrezkov <ghaiklor@gmail.com>
2021-06-20 14:22:16 +0300

    Update plugins installer

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,38 +1,26 @@
 #!/usr/bin/env fish
 #title          :install_plugins.sh
-#description    :This script will install and configure themes and plugins for Fish Shell.
+#description    :This script will install and configure themes and plugins for Fish Shell
 #author         :ghaiklor
-#date           :2017-12-11
-#version        :0.1
+#date           :2021-06-20
+#version        :1.0
 #usage          :curl -s https://raw.githubusercontent.com/ghaiklor/iterm-fish-fisher-osx/master/install_plugins.sh | fish
-#fish_version   :2.7.0
+#fish_version   :3.2.2
 #===================================================================================
 
-fisher install igalic/anicode
+brew install terminal-notifier grc
+
 fisher install edc/bass
 fisher install oh-my-fish/theme-bobthefish
-fisher install laughedelic/brew-completions
-fisher install patrickf3139/colored_man_pages.fish
-
-brew install terminal-notifier
+fisher install patrickf1/colored_man_pages.fish
 fisher install franciscolourenco/done
-
-fisher install Shadowigor/plugin-errno-grep
-
-brew install fzy
-fisher install gyakovlev/fish-fzy
-
-brew install grc
 fisher install oh-my-fish/plugin-grc
-
-fisher install vincentjames501/fish-kill-on-port
-fisher install oh-my-fish/plugin-node-binpath
 fisher install jorgebucaran/nvm.fish
-
 fisher install oh-my-fish/plugin-pj
-set -Ux PROJECT_PATHS ~/Library/Projects
-
-fisher install oh-my-fish/plugin-rustup
 fisher install markcial/upto
+
+set --universal --export theme_nerd_fonts yes
+set --universal --export theme_color_scheme zenburn
+set --universal --export PROJECT_PATHS ~/Library/Projects
 
 fish_update_completions
